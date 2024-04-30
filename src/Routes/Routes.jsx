298b4aaa7ad6da8dcad48818174f749dc52c0e08
Dipro <dipropaul032@gamil.com>
2024-05-01 02:10:11 +0600

    added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Land from "../page/Land";
+import OurStory from "../page/OurStory";
+import OurImpact from "../page/OurImpact";
+import Press from "../page/Press";
+import GiftCard from "../page/GiftCard";
 
 export const router = createBrowserRouter([{
     path: "/",
@@ -9,6 +13,22 @@ export const router = createBrowserRouter([{
         {
             path: "/",
             element: <Land></Land>
+        },
+        {
+            path: "/ourstory",
+            element: <OurStory></OurStory>
+        },
+        {
+            path: "/ourimpact",
+            element: <OurImpact></OurImpact>
+        },
+        {
+            path: "/press",
+            element: <Press></Press>
+        },
+        {
+            path: "/giftcard",
+            element: <GiftCard></GiftCard>
         },
        ]
 }])
